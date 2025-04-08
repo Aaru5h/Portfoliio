f779d09d7eb2263efa342fabd69cd5a8cbc60600
Aaru5h <mestrikerocks@gmail.com>
2025-04-08 16:48:03 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Home from './components/Home';
+import Projects from './assets/Projects'
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Projects />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,2 +1,10 @@
 import React,{useState,useEffect} from 'raect'
 import { Github, Mail, Linkedin, Menu, X, ChevronDown } from 'lucide-react';
+
+export default Portfolio = ()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
